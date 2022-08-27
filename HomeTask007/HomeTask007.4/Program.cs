8321a0aac2cc0bf3d217772cfd514b6877241704
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-28 01:28:53 +0500

    Added changes.

--- HomeTask007/HomeTask007.4/Program.cs
+++ HomeTask007/HomeTask007.4/Program.cs
@@ -13,28 +13,31 @@ string CR()
 
 try
 {
-  CW("Задание 5. Программа, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.");
+  CW("Задание 4. Программа, которая находит сумму элементов, находящихся на главной диагонали.");
 
-  CW("Введите позицию элемента массива:");
-
-  int[] position = new int[2];
-
-  for (int i = 0; i < position.Length; i++) position[i] = Int32.Parse(CR());
-
-  int rows = new Random().Next(1, 6);
-  int columns = new Random().Next(1, 6);
-  int[,] array = new int[rows, columns];
+  CW("Введите количество строк массива: ");
+  int rows = Int32.Parse(CR());
+  CW("Введите количество столбцов массива: ");
+  int colums = Int32.Parse(CR());
+  int[,] array = new int[rows, colums];
 
   for (int i = 0; i < rows; i++)
   {
-    for (int j = 0; j < columns; j++)
+    for (int j = 0; j < colums; j++)
     {
       array[i, j] = new Random().Next(-10, 10);
       CWR(array[i, j].ToString() + " ");
     }
     CW("");
   }
-
-  CW($"Значение элемента: {(position[0] < rows && position[1] < columns ? array[position[0] - 1, position[1] - 1] + "." : "отсутствует.")}");
+  int sum = 0;
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < colums; j++)
+    {
+      if (i == j) sum = sum + array[i, j];
+    }
+  }
+  CW($"Сумма всех элементов, находящихся на главной диагонали = {sum.ToString()}");
 }
 catch (InvalidCastException e) { }
