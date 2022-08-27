d06a8f114e37b6dd058c7486e09adcfc29d5c702
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-28 01:29:16 +0500

    HomeTask007.5 completed.

--- HomeTask007/HomeTask007.5/Program.cs
+++ HomeTask007/HomeTask007.5/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CWR(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 5. Программа, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.");
+
+  CW("Введите позицию элемента массива:");
+
+  int[] position = new int[2];
+
+  for (int i = 0; i < position.Length; i++) position[i] = Int32.Parse(CR());
+
+  int rows = new Random().Next(1, 6);
+  int columns = new Random().Next(1, 6);
+  int[,] array = new int[rows, columns];
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      array[i, j] = new Random().Next(-10, 10);
+      CWR(array[i, j].ToString() + " ");
+    }
+    CW("");
+  }
+
+  CW($"Значение элемента: {(position[0] < rows && position[1] < columns ? array[position[0] - 1, position[1] - 1] + "." : "отсутствует.")}");
+}
+catch (InvalidCastException e) { }
