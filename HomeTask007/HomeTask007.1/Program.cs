ecc1ad081d666baa22892f93e706778151d8b331
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-28 00:13:22 +0500

    HomeTask007.1 completed.

--- HomeTask007/HomeTask007.1/Program.cs
+++ HomeTask007/HomeTask007.1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CWR(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 1. Программа, которая задает двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m + n.");
+  CW("Введите количество строк массива: ");
+  int rows = Int32.Parse(CR());
+  CW("Введите количество столбцов массива: ");
+
+  int colums = Int32.Parse(CR());
+  int[,] array = new int[rows, colums];
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < colums; j++)
+    {
+      array[i, j] = i + j;
+      CWR($"{array[i, j]}  ");
+    }
+    CW("");
+  }
+
+}
+catch (InvalidCastException e) { }
